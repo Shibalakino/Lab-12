5e675b079233eff33bc1b2bb1bf8bc8b6f2d837e
Oleg Boyko <57860598+Shibalakino@users.noreply.github.com>
2020-05-08 04:42:55 +0300

    Add files via upload

---
+++ Lab12.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <fstream>
+ using namespace std;
+int main() {
+    char str[300];
+    cout << "Write something \n";
+    cin >> str;
+    ofstream test;
+    test.open("Example.txt");
+    test << str;
+    int i = 0;
+    while (str[i]) i++;
+    printf("%i\n", i);
+    return 0;
+}
